3fcbd8c8c094f5928ade7c1f8db2f9caa0bb0654
Hypobeggar <89600337+Hypobeggar@users.noreply.github.com>
2023-04-22 18:07:40 -0500

    Finish splash screen

--- app/src/main/java/com/example/mangalibrary/SplashScreen.java
+++ app/src/main/java/com/example/mangalibrary/SplashScreen.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 public class SplashScreen extends AppCompatActivity {
@@ -12,12 +13,22 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
 
-        Log.i("Notice", "Timer end");
+                Intent startHome = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(startHome);
+                finish();
+            }
+
+        }, 3000);
+
     }
     @Override
     protected void onStart() {
         super.onStart();
+
 
     }
 }
